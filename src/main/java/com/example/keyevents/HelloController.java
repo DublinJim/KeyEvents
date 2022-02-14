6bc7ee0d5301766b66faa5852a2aee7146731baa
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-02-14 15:20:46 -0500

    adds an event

--- src/main/java/com/example/keyevents/HelloController.java
+++ src/main/java/com/example/keyevents/HelloController.java
@@ -3,12 +3,24 @@ package com.example.keyevents;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.text.Text;
 
 public class HelloController {
+  @FXML private Text txt1;
 
-    @FXML
-    void closeApp(ActionEvent event) {
-        Platform.exit ();
+  @FXML
+  void closeApp(ActionEvent event) {
+    Platform.exit();
+  }
+
+  @FXML
+  void whatsTheKey(KeyEvent event) {
+    if (event.getCode() == KeyCode.ENTER) {
+
+      txt1.setText("You pressed enter");
     }
 
+  }
 }
