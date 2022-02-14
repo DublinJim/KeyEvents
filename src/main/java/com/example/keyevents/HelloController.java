7739a8355cab109d62283fc2372da76867174b89
DublinJim <59180380+DublinJim@users.noreply.github.com>
2022-02-14 15:07:20 -0500

    initial commit ... clean interface

--- src/main/java/com/example/keyevents/HelloController.java
+++ src/main/java/com/example/keyevents/HelloController.java
@@ -1,14 +1,14 @@
 package com.example.keyevents;
 
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText ("Welcome to JavaFX Application!");
+    void closeApp(ActionEvent event) {
+        Platform.exit ();
     }
+
 }
